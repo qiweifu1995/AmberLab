0eafde765a477e3e0b94b62aa1a0b6f52b02cbf6
qiweifu1995 <qiweif@uci.edu>
2020-09-18 11:08:52 -0700

    End of day commit,
    Not beta version, adding new tracking for UI state for efficient update

--- Helper.py
+++ Helper.py
@@ -132,6 +132,141 @@ def histogram_bin(range_max, increment):
     return bin_edge
 
 
+class ui_state:
+    """this class holds all the current state of the UI"""
+    def __init__(self):
+        self.all_check = False
+        self.ch1_check = False
+        self.ch2_check = False
+        self.ch3_check = False
+        self.ch1_2_check = False
+        self.ch1_3_check = False
+        self.ch2_3_check = False
+        self.file_select = -1
+        self.gating_channel_select = -1
+        self.gating_bins = 0
+        self.gating_voltage = 0
+        self.scatter_channel_select_x = -1
+        self.scatter_channel_select_y = -1
+        self.scatter_gate_voltage_x = 0
+        self.scatter_gate_voltage_y = 0
+        self.sweep_channel_select = -1
+        self.sweep_bins = 0
+        self.sweep_file_1 = -1
+        self.sweep_file_2 = -1
+
+    def working_file_update_check(self, update_state=True, file=None, chall=None, ch1=None, ch2=None,
+                                  ch3=None, ch1_2=None, ch1_3=None, ch2_3=None):
+        """checks if checkbox are updated and needs to be refreshed"""
+        changed = False
+        if file is not None and self.file_select != file:
+            changed = True
+        elif chall is not None and self.all_check != chall:
+            changed = True
+        elif ch1 is not None and self.ch1_check != ch1:
+            changed = True
+        elif ch2 is not None and self.ch2_check != ch2:
+            changed = True
+        elif ch3 is not None and self.ch3_check != ch3:
+            changed = True
+        elif ch1_2 is not None and self.ch1_2_check != ch1_2:
+            changed = True
+        elif ch1_3 is not None and self.ch1_3_check != ch1_3:
+            changed = True
+        elif ch2_3 is not None and self.ch2_3_check != ch2_3:
+            changed = True
+        if update_state:
+            if file is not None:
+                self.file_select = file
+            if chall is not None:
+                self.all_check = chall
+            if ch1 is not None:
+                self.ch1_check = ch1
+            if ch2 is not None:
+                self.ch2_check = ch2
+            if ch3 is not None:
+                self.ch3_check = ch3
+            if ch1_2 is not None:
+                self.ch1_2_check = ch1_2
+            if ch1_3 is not None:
+                self.ch1_3_check = ch1_3
+            if ch2_3 is not None:
+                self.ch2_3_check = ch2_3
+        return changed
+
+    def gating_update(self, update_state=True, channel_select=None, bins=None, gate_voltage=None):
+        """keeps track of states in gating"""
+        replot, recalculate = False
+        if channel_select is not None and channel_select != self.gating_channel_select:
+            replot = True
+        elif bins is not None and bins != self.gating_bins:
+            replot = True
+        if gate_voltage is not None and gate_voltage != self.gating_voltage:
+            recalculate = True
+        if update_state:
+            if channel_select is not None:
+                self.gating_channel_select = channel_select
+            if bins is not None:
+                self.gating_bins = bins
+            if gate_voltage is not None:
+                self.gating_voltage = gate_voltage
+        return replot, recalculate
+
+    def scatter_update(self, update_state=True, x_select=None, y_select=None, x_gate=None, y_gate=None):
+        """keep track of state in scatter tab"""
+        replot, recalculate = False
+        if x_select is not None and x_select != self.scatter_channel_select_x:
+            replot = True
+        elif y_select is not None and y_select != self.scatter_channel_select_y:
+            replot = True
+        if x_gate is not None and x_gate != self.scatter_gate_voltage_x:
+            recalculate = True
+        elif y_gate is not None and y_gate != self.scatter_gate_voltage_y:
+            recalculate = True
+        if update_state:
+            if x_select is not None:
+                self.scatter_channel_select_x = x_select
+            if y_select is not None:
+                self.scatter_channel_select_y = y_select
+            if x_gate is not None:
+                self.scatter_gate_voltage_x = x_gate
+            if y_gate is not None:
+                self.scatter_gate_voltage_y = y_gate
+        return replot, recalculate
+
+    def sweep_update(self, update_state=False, channel_select=None, bins=None, file1=None, file2=None):
+        """keep track of state in sweep tab"""
+        replot1, replot2 = False
+        if file1 is not None and file1 != self.sweep_file_1:
+            replot1 = True
+        if file2 is not None and file2 != self.sweep_file_2:
+            replot2 = True
+        if channel_select is not None and channel_select != self.sweep_channel_select:
+            replot1 = True
+            replot2 = True
+        elif bins is not None and bins != self.sweep_bins:
+            replot1 = True
+            replot2 = True
+        if update_state:
+            if channel_select is not None:
+                self.sweep_channel_select = channel_select
+            if bins is not None:
+                self.sweep_bins = bins
+            if file1 is not None:
+                self.sweep_file_1 = file1
+            if file2 is not None:
+                self.sweep_file_2 = file2
+        return replot1, replot2
+
+
+
+
+if __name__ == "__main__":
+    state = ui_state()
+    update = state.checkbox_update_check(True,ch2=True)
+    print(update)
+    print(state.ch2_check)
+    print(state.ch1_check)
 
 
 
